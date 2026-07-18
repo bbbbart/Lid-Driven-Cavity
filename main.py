6495af61ec9ca474c8b3b1d3d9dd83cb8f1d9e54
bdabkowski1 <bdabkowski353@gmail.com>
2026-07-17 23:59:21 -0500

    Start implementing boundary conditions

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ u = np.zeros((100,100))
 v = np.zeros((100,100))
 p = np.zeros((100,100))
 
+wall_v = 1.0
+
 ghost_u = np.zeros((102,102))
 ghost_v = np.zeros((102,102))
 ghost_p = np.zeros((102,102))
@@ -27,35 +29,49 @@ contour = ax.contour(x, y, np.sqrt(v*v + u*u))
 def updateGrid(frame):
     global p, u, v, contour, splot
     in_tolerance = False            
-    u[0:1] = 1
 
     ghost_u[1:-1, 1:-1] = u
     ghost_v[1:-1, 1:-1] = v
     ghost_p[1:-1, 1:-1] = p
+    ghost_p[0::len(ghost_p)-1, 1:-1] =  p[0::len(p)-1]
+    ghost_p[1:-1, 0::len(ghost_p)-1] = p[:, 0::len(p)-1]
 
     up_u = ghost_u[:-2, 1:-1] 
     down_u = ghost_u[2:, 1:-1] 
     left_u = ghost_u[1:-1, :-2] 
     right_u = ghost_u[1:-1, 2:] 
+    ghost_u[0::, 1::len(u)-2] =  2*wall_v - u[0::]
+    ghost_u[::-1] = -u[::-1]
+    ghost_u[1:-1, 0::len(ghost_u)-1] = -u[:, 0::len(u)-1]
+
 
     up_v = ghost_v[:-2, 1:-1] 
     down_v = ghost_v[2:, 1:-1] 
     left_v = ghost_v[1:-1, :-2] 
     right_v = ghost_v[1:-1, 2:] 
+    ghost_v[0::, 1::len(u)-2] =  2*wall_v - v[0::]
+    ghost_v[0::, 1::len(u)-2] = -v[::-1]
+    ghost_v[1:-1, 0::len(ghost_v)-1] = -v[:, 0::len(v)-1]
 
     up_p = ghost_p[:-2, 1:-1] 
     down_p = ghost_p[2:, 1:-1] 
     left_p = ghost_p[1:-1, :-2] 
     right_p = ghost_p[1:-1, 2:] 
 
-    u = u + t*(-u*(left_u - right_u)/2 - v*(up_u - down_u)/2) + (1/re)*(left_u + right_u + up_u + down_u - 4*u) ## horiz velocity
-    v = v + t*(-u*(left_v - right_v)/2 - v*(up_v - down_v)/2) + (1/re)*(left_v + right_v + up_v + down_v - 4*u) ## vert velocity
+    old_v = v
+    old_u = u
+    
+    u = old_u + t*(-u*(left_u - right_u)/2 - v*(up_u - down_u)/2) + (1/re)*(left_u + right_u + up_u + down_u - 4*u) ## horiz velocity
+    v = old_v + t*(-old_u*(left_v - right_v)/2 - old_v*(up_v - down_v)/2) + (1/re)*(left_v + right_v + up_v + down_v - 4*u) ## vert velocity
     b = (1/t)*(right_u - left_u + up_v - down_v)/2 ## check error
 
     while not in_tolerance:
         p_new = (up_p + down_p + left_p + right_p - b)/4 ## pressure solver
         if (p_new[50,50] - p[50,50]) > tolerance:
             p = p_new
+
+            ghost_p[0::len(ghost_p)-1, 1:-1] =  p[0::len(p)-1]
+            ghost_p[1:-1, 0::len(ghost_p)-1] = p[:, 0::len(p)-1]
 
             ghost_p[1:-1, 1:-1] = p
             up_p = ghost_p[:-2, 1:-1] 
@@ -67,7 +83,7 @@ def updateGrid(frame):
         else:
             in_tolerance = True
             p = p_new
-    
+
     u = u - t*(right_p - left_p)/2 ## correction
     v = v - t*(up_p - down_p)/2 ## correction
     

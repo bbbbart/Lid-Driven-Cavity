6c160ca060a078ba0b78c048c90c350f1f031786
bdabkowski1 <bdabkowski353@gmail.com>
2026-07-18 21:25:06 -0500

    Bug fixes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 re = 10
-t = 1
+t = 0.25
 
 u = np.zeros((100,100))
 v = np.zeros((100,100))
@@ -30,28 +30,28 @@ def updateGrid(frame):
     global p, u, v, contour, splot
     in_tolerance = False            
 
-    ghost_u[1:-1, 1:-1] = u
-    ghost_v[1:-1, 1:-1] = v
     ghost_p[1:-1, 1:-1] = p
     ghost_p[0::len(ghost_p)-1, 1:-1] =  p[0::len(p)-1]
     ghost_p[1:-1, 0::len(ghost_p)-1] = p[:, 0::len(p)-1]
+
+    ghost_v[1:-1, 1:-1] = v
+    ghost_v[0::len(ghost_v)-1, 1:-1] = -v[0::len(v)-1]
+    ghost_v[1:-1, 0::len(ghost_v)-1] = -v[:, 0::len(v)-1]
+    
+    ghost_u[1:-1, 1:-1] = u
+    ghost_u[0, 1:len(ghost_u)-1] =  -u[0,:] + 2*wall_v
+    ghost_u[1:-1, 0::len(ghost_u)-1] = -u[:, 0::len(u)-1]
 
     up_u = ghost_u[:-2, 1:-1] 
     down_u = ghost_u[2:, 1:-1] 
     left_u = ghost_u[1:-1, :-2] 
     right_u = ghost_u[1:-1, 2:] 
-    ghost_u[0::, 1::len(u)-2] =  2*wall_v - u[0::]
-    ghost_u[::-1] = -u[::-1]
-    ghost_u[1:-1, 0::len(ghost_u)-1] = -u[:, 0::len(u)-1]
-
 
     up_v = ghost_v[:-2, 1:-1] 
     down_v = ghost_v[2:, 1:-1] 
     left_v = ghost_v[1:-1, :-2] 
     right_v = ghost_v[1:-1, 2:] 
-    ghost_v[0::, 1::len(u)-2] =  2*wall_v - v[0::]
-    ghost_v[0::, 1::len(u)-2] = -v[::-1]
-    ghost_v[1:-1, 0::len(ghost_v)-1] = -v[:, 0::len(v)-1]
+
 
     up_p = ghost_p[:-2, 1:-1] 
     down_p = ghost_p[2:, 1:-1] 
@@ -62,7 +62,7 @@ def updateGrid(frame):
     old_u = u
     
     u = old_u + t*(-u*(left_u - right_u)/2 - v*(up_u - down_u)/2) + (1/re)*(left_u + right_u + up_u + down_u - 4*u) ## horiz velocity
-    v = old_v + t*(-old_u*(left_v - right_v)/2 - old_v*(up_v - down_v)/2) + (1/re)*(left_v + right_v + up_v + down_v - 4*u) ## vert velocity
+    v = old_v + t*(-old_u*(left_v - right_v)/2 - old_v*(up_v - down_v)/2) + (1/re)*(left_v + right_v + up_v + down_v - 4*v) ## vert velocity
     b = (1/t)*(right_u - left_u + up_v - down_v)/2 ## check error
 
     while not in_tolerance:
@@ -79,7 +79,7 @@ def updateGrid(frame):
             left_p = ghost_p[1:-1, :-2] 
             right_p = ghost_p[1:-1, 2:] 
 
-            p_new = (up_p + down_p + left_p + right_p - b)/4 ## pressure solver
+            p_new = (up_p + down_p + left_p + right_p - b)/2 ## pressure solver
         else:
             in_tolerance = True
             p = p_new
@@ -87,7 +87,7 @@ def updateGrid(frame):
     u = u - t*(right_p - left_p)/2 ## correction
     v = v - t*(up_p - down_p)/2 ## correction
     
-    fig.clf()
+    ax.clear()
     contour = ax.contour(x, y, np.sqrt(v*v + u*u))
     splot = plt.streamplot(x, y, u, v, cmap='viridis')
 

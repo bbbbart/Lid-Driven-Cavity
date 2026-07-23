08deb42190b9a9cb8a27a94a4b1a4c7321365d42
bdabkowski1 <bdabkowski353@gmail.com>
2026-07-22 20:50:45 -0500

    Final changes before switching pressure solvers

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 
 re = 10
-t = 0.01
+t = 0.005
 wall_v = 1.0
-tolerance = 0.01
-max_i = 200
+tolerance = 0.0001
+max_i = 2000
 delta_x = 1.0
 delta_y = 1.0
 h = delta_x
@@ -26,18 +26,24 @@ fig, ax = plt.subplots()
 fig.set_size_inches(8,6)
 fig.canvas.manager.set_window_title("Lid Cavity")
 
-quiv = ax.quiver(x[::10], y[::10], u[::10, ::10], v[::10, ::10], scale=15)
-##splot = plt.streamplot(x, y, u, v, cmap='viridis')
-##contour = ax.contour(x, y, np.sqrt(v*v + u*u))
+quiv = ax.quiver(x[::5], y[::5], u[::5, ::5], v[::5, ::5], scale=25)
 
 def updateGrid(frame):
     global p, u, v, quiv
 
-    for i in range(10):
+    for i in range(150):
+        u[0,:] = 1.0
+        v[0,:] = 0.0
+
         ghost_v[1:-1, 1:-1] = v
         ghost_v[[0, -1], 1:-1] = -v[[0, -1], :]
         ghost_v[1:-1, [0, -1]] = -v[:, [0, -1]]
         
+        up_v = ghost_v[:-2, 1:-1] 
+        down_v = ghost_v[2:, 1:-1] 
+        left_v = ghost_v[1:-1, :-2] 
+        right_v = ghost_v[1:-1, 2:] 
+
         ghost_u[1:-1, 1:-1] = u
         ghost_u[0, 1:-1] =  -1*(u[0,:]) + 2*wall_v
         ghost_u[-1, 1:-1] = -u[-1,:]
@@ -46,12 +52,7 @@ def updateGrid(frame):
         up_u = ghost_u[:-2, 1:-1] 
         down_u = ghost_u[2:, 1:-1] 
         left_u = ghost_u[1:-1, :-2] 
-        right_u = ghost_u[1:-1, 2:] 
-
-        up_v = ghost_v[:-2, 1:-1] 
-        down_v = ghost_v[2:, 1:-1] 
-        left_v = ghost_v[1:-1, :-2] 
-        right_v = ghost_v[1:-1, 2:] 
+        right_u = ghost_u[1:-1, 2:]
 
         old_v = v.copy()
         old_u = u.copy()
@@ -59,15 +60,18 @@ def updateGrid(frame):
         u = old_u + t*((-old_u*(right_u - left_u )/(2*delta_x) - old_v*(down_u - up_u)/(2*delta_y)) + (1/re)*(((left_u - 2*old_u + right_u)/delta_x**2)+((up_u - 2*old_u + down_u)/delta_y**2))) ## horiz velocity
         v = old_v + t*((-old_u*(right_v - left_v)/(2*delta_x) - old_v*(down_v - up_v)/(2*delta_y)) + (1/re)*(((left_v - 2*old_v + right_v)/delta_x**2)+((up_v - 2*old_v + down_v)/delta_y**2))) ## horiz velocity
         
+        u[0,:] = 1.0
+        v[0,:] = 0.0
+
         ghost_v[1:-1, 1:-1] = v
         ghost_v[[0, -1], 1:-1] = -v[[0, -1], :]
         ghost_v[1:-1, [0, -1]] = -v[:, [0, -1]]
-        
+
         ghost_u[1:-1, 1:-1] = u
         ghost_u[0, 1:-1] =  -1*(u[0,:]) + 2*wall_v
         ghost_u[-1, 1:-1] = -u[-1,:]
-        ghost_u[1:-1, [0, -1]] = -u[:, [0, -1]]
-        
+        ghost_u[1:-1, [0, -1]] = -u[:, [0, -1]]        
+
         left_u = ghost_u[1:-1, :-2] 
         right_u = ghost_u[1:-1, 2:] 
 
@@ -76,20 +80,20 @@ def updateGrid(frame):
 
         b = (1/t)*((right_u - left_u)/(2*delta_x) + (down_v - up_v)/(2*delta_y)) ## check error
         for i in range(max_i):
-            ghost_p[[0,-1], 1:-1] =  p[[0, -1], :]
-            ghost_p[1:-1, [0, -1]] = p[:, [0, -1]]
-            ghost_p[1:-1, 1:-1] = p
+            p_old = p.copy()
+
+            ghost_p[[0,-1], 1:-1] =  p_old[[0, -1], :]
+            ghost_p[1:-1, [0, -1]] = p_old[:, [0, -1]]
+            ghost_p[1:-1, 1:-1] = p_old
 
             up_p = ghost_p[:-2, 1:-1] 
             down_p = ghost_p[2:, 1:-1] 
             left_p = ghost_p[1:-1, :-2] 
             right_p = ghost_p[1:-1, 2:] 
-
-            p_new = (up_p + down_p + left_p + right_p - (h**2)*b)/4 ## pressure solver
-            if (np.max(np.abs(p_new - p)) < tolerance):
-                p = p_new
+       
+            p = (up_p + down_p + left_p + right_p - (h**2)*b)/4 ## pressure solver
+            if (np.max(np.abs(p - p_old)) < tolerance):
                 break
-        p = p_new
 
         ghost_p[[0,-1], 1:-1] =  p[[0, -1], :]
         ghost_p[1:-1, [0, -1]] = p[:, [0, -1]]
@@ -102,14 +106,9 @@ def updateGrid(frame):
 
         u = u - t*(right_p - left_p)/(2*delta_x) ## correction
         v = v - t*(down_p - up_p)/(2*delta_y) ## correction
-    
-    quiv.set_UVC(u[::10, ::10], v[::10, ::10])
-    ##ax.clear()
-    ##contour = ax.contour(x, y, np.sqrt(v**2 + u**2))
-    ##splot = plt.streamplot(x, y, u, v, cmap='viridis')
 
-    ##return(contour,splot)
+    quiv.set_UVC(u[::5, ::5], v[::5, ::5])
     return(quiv)
 
-ani = animation.FuncAnimation(fig, updateGrid, frames = 999, interval = 50)
+ani = animation.FuncAnimation(fig, updateGrid, frames = 999, interval = 25)
 plt.show()

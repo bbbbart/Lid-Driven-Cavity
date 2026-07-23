a7034a5e3921771780e8b4cd88d8dda2c1cc3457
bdabkowski1 <bdabkowski353@gmail.com>
2026-07-22 23:08:56 -0500

    Replace Jacobi with Gauss Seidel method

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ re = 10
 t = 0.005
 wall_v = 1.0
 tolerance = 0.0001
-max_i = 2000
+max_i = 2500
 delta_x = 1.0
 delta_y = 1.0
 h = delta_x
@@ -26,19 +26,18 @@ fig, ax = plt.subplots()
 fig.set_size_inches(8,6)
 fig.canvas.manager.set_window_title("Lid Cavity")
 
-quiv = ax.quiver(x[::5], y[::5], u[::5, ::5], v[::5, ::5], scale=25)
+quiv = ax.quiver(x[::10], y[::10], u[::10, ::10], v[::10, ::10], scale=25)
 
 def updateGrid(frame):
-    global p, u, v, quiv
-
     for i in range(150):
+        global p, u, v, quiv
         u[0,:] = 1.0
         v[0,:] = 0.0
 
         ghost_v[1:-1, 1:-1] = v
         ghost_v[[0, -1], 1:-1] = -v[[0, -1], :]
         ghost_v[1:-1, [0, -1]] = -v[:, [0, -1]]
-        
+
         up_v = ghost_v[:-2, 1:-1] 
         down_v = ghost_v[2:, 1:-1] 
         left_v = ghost_v[1:-1, :-2] 
@@ -56,10 +55,10 @@ def updateGrid(frame):
 
         old_v = v.copy()
         old_u = u.copy()
-        
+
         u = old_u + t*((-old_u*(right_u - left_u )/(2*delta_x) - old_v*(down_u - up_u)/(2*delta_y)) + (1/re)*(((left_u - 2*old_u + right_u)/delta_x**2)+((up_u - 2*old_u + down_u)/delta_y**2))) ## horiz velocity
         v = old_v + t*((-old_u*(right_v - left_v)/(2*delta_x) - old_v*(down_v - up_v)/(2*delta_y)) + (1/re)*(((left_v - 2*old_v + right_v)/delta_x**2)+((up_v - 2*old_v + down_v)/delta_y**2))) ## horiz velocity
-        
+
         u[0,:] = 1.0
         v[0,:] = 0.0
 
@@ -79,25 +78,23 @@ def updateGrid(frame):
         down_v = ghost_v[2:, 1:-1] 
 
         b = (1/t)*((right_u - left_u)/(2*delta_x) + (down_v - up_v)/(2*delta_y)) ## check error
+
         for i in range(max_i):
-            p_old = p.copy()
+            error = 0
+            p = ghost_p[1:-1, 1:-1]
+            ghost_p[[0,-1], 1:-1] =  p[[0, -1], :]
+            ghost_p[1:-1, [0, -1]] = p[:, [0, -1]]
+            ghost_p[1:-1, 1:-1] = p
+            for y in range(1, 100):
+                for x in range(1, 100):
+                    old_p = ghost_p[y,x]
 
-            ghost_p[[0,-1], 1:-1] =  p_old[[0, -1], :]
-            ghost_p[1:-1, [0, -1]] = p_old[:, [0, -1]]
-            ghost_p[1:-1, 1:-1] = p_old
+                    ghost_p[y,x] = (ghost_p[y + 1, x] + ghost_p[y - 1, x] + ghost_p[y, x - 1] + ghost_p[y, x + 1] - (h**2)*b[y,x])/4 ## pressure solver
+             
+                    error = max(error, abs(ghost_p[y,x]-old_p))
 
-            up_p = ghost_p[:-2, 1:-1] 
-            down_p = ghost_p[2:, 1:-1] 
-            left_p = ghost_p[1:-1, :-2] 
-            right_p = ghost_p[1:-1, 2:] 
-       
-            p = (up_p + down_p + left_p + right_p - (h**2)*b)/4 ## pressure solver
-            if (np.max(np.abs(p - p_old)) < tolerance):
+            if (error < tolerance):
                 break
-
-        ghost_p[[0,-1], 1:-1] =  p[[0, -1], :]
-        ghost_p[1:-1, [0, -1]] = p[:, [0, -1]]
-        ghost_p[1:-1, 1:-1] = p
 
         up_p = ghost_p[:-2, 1:-1] 
         down_p = ghost_p[2:, 1:-1] 
@@ -107,7 +104,7 @@ def updateGrid(frame):
         u = u - t*(right_p - left_p)/(2*delta_x) ## correction
         v = v - t*(down_p - up_p)/(2*delta_y) ## correction
 
-    quiv.set_UVC(u[::5, ::5], v[::5, ::5])
+    quiv.set_UVC(u[::10, ::10], v[::10, ::10])
     return(quiv)
 
 ani = animation.FuncAnimation(fig, updateGrid, frames = 999, interval = 25)

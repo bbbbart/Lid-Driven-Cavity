6c209576d7aeb5ed8d155f978dc3258a5f209483
bdabkowski1 <bdabkowski353@gmail.com>
2026-07-27 00:24:33 -0500

    Add pressure map

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
+import matplotlib.widgets as widget
 from numba import jit
 
 re = 10
 t = 0.005
 wall_v = 1.0
 tolerance = 0.0001
-max_i = 300
+max_i = 1000
 delta_x = 1.0
 delta_y = 1.0
 h = delta_x
@@ -23,16 +24,21 @@ ghost_p = np.zeros((102,102))
 x = np.linspace(50, -50, 100)
 y = np.linspace(50, -50, 100)
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(2, 2)
 fig.set_size_inches(8,6)
-fig.canvas.manager.set_window_title("Lid Cavity")
+fig.canvas.manager.set_window_title("Velocity Quiver Plot")
+ax[0,0].set_aspect('equal')
+ax[0,1].set_aspect('equal')
 
-quiv = ax.quiver(x[::10], y[::10], u[::10, ::10], v[::10, ::10], scale=15)
+quiv = ax[0,0].quiver(x[::10], y[::10], u[::10, ::10], v[::10, ::10], scale=15, pivot='mid')
+pressure = ax[0, 1].imshow(p, cmap='viridis', extent=[-50, 50, -50, 50], interpolation = 'bilinear')
+plt.colorbar(pressure)
 
 @jit
 def pressureSolver(grid, ghost, b_):
     for i in range(max_i):
         error = 0
+        ghost[0,0] = 0
         ghost[0, 1:-1] =  grid[0, :]
         ghost[-1, 1:-1] =  grid[-1, :]
         ghost[1:-1, -1] = grid[:, -1]
@@ -45,12 +51,20 @@ def pressureSolver(grid, ghost, b_):
                 ghost[y,x] = ghost[y,x] + 1.7*(g - ghost[y,x])
 
                 error = max(error, abs(ghost[y,x]-old_p))
-
         if (error < tolerance):
+            ghost[0,0] = 0
+            ghost[0, 1:-1] =  grid[0, :]
+            ghost[-1, 1:-1] =  grid[-1, :]
+            ghost[1:-1, -1] = grid[:, -1]
+            ghost[1:-1, 0] = grid[:, 0]
             return ghost, grid
             break
+    ghost[0,0] = 0
+    ghost[0, 1:-1] =  grid[0, :]
+    ghost[-1, 1:-1] =  grid[-1, :]
+    ghost[1:-1, -1] = grid[:, -1]
+    ghost[1:-1, 0] = grid[:, 0]
     return ghost, grid
-
 
 def updateGrid(frame):
     for i in range(250):
@@ -80,8 +94,8 @@ def updateGrid(frame):
         old_v = v.copy()
         old_u = u.copy()
 
-        u = old_u + t*((-old_u*(right_u - left_u )/(2*delta_x) - old_v*(down_u - up_u)/(2*delta_y)) + (1/re)*(((left_u - 2*old_u + right_u)/delta_x**2)+((up_u - 2*old_u + down_u)/delta_y**2))) ## horiz velocity
-        v = old_v + t*((-old_u*(right_v - left_v)/(2*delta_x) - old_v*(down_v - up_v)/(2*delta_y)) + (1/re)*(((left_v - 2*old_v + right_v)/delta_x**2)+((up_v - 2*old_v + down_v)/delta_y**2))) ## horiz velocity
+        u = old_u + t*(-old_u*(np.where(old_u >= 0, (old_u - left_u )/delta_x, (right_u - old_u)/delta_x)) - old_v*(np.where(old_v >= 0, (old_u - up_u)/delta_y, (down_u - old_u)/delta_y)) + (1/re)*(((left_u - 2*old_u + right_u)/delta_x**2) + ((up_u - 2*old_u + down_u)/delta_y**2))) ## horiz velocity
+        v = old_v + t*(-old_u*(np.where(old_u >= 0, (old_v - left_v )/delta_x, (right_v - old_v)/delta_x)) - old_v*(np.where(old_v >= 0, (old_v - up_v)/delta_y, (down_v - old_v)/delta_y)) + (1/re)*(((left_v - 2*old_v + right_v)/delta_x**2) + ((up_v - 2*old_v + down_v)/delta_y**2))) ## vert velocity
 
         u[0,:] = 1.0
         v[0,:] = 0.0
@@ -104,18 +118,19 @@ def updateGrid(frame):
         b = (1/t)*((right_u - left_u)/(2*delta_x) + (down_v - up_v)/(2*delta_y)) ## check error
 
         ghost_p, p = pressureSolver(p, ghost_p, b)
-        p = ghost_p[1:-1, 1:-1]
 
+        p = ghost_p[1:-1, 1:-1]
         up_p = ghost_p[:-2, 1:-1] 
         down_p = ghost_p[2:, 1:-1] 
         left_p = ghost_p[1:-1, :-2] 
-        right_p = ghost_p[1:-1, 2:] 
+        right_p = ghost_p[1:-1, 2:]
 
         u = u - t*(right_p - left_p)/(2*delta_x) ## correction
         v = v - t*(down_p - up_p)/(2*delta_y) ## correction
 
     quiv.set_UVC(u[::10, ::10], v[::10, ::10])
-    return(quiv)
+    pressure.set_data(p)
+    return(pressure,)
 
 ani = animation.FuncAnimation(fig, updateGrid, frames = 999, interval = 25)
 plt.show()
